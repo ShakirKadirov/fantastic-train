b5b6f63cd49389f2a6fb094df88773cdfddc5930
ShakirKadirov <shakirkadirov1@gmail.com>
2024-04-26 01:22:57 +0500

    Initial Commit

---
+++ testingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testingProject
+//
+//  Created by Shakir Kadirov on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
